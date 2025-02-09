756115492b00e7846c99a9a9fa6b17d2564a8d7a
Mohamad Smeha <202210810@ua.edu.lb>
2025-02-09 11:56:20 +0200

    minor changes

--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import GenderCheckbox from "./GenderCheckbox";
 import useSignup from "../../hooks/useSignup";
